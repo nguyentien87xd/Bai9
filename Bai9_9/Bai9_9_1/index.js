92930a6b1f043477f472def7c7aa009cb2df8f12
nguyentien87xd <130019768+nguyentien87xd@users.noreply.github.com>
2023-04-23 23:48:03 +0700

    Delete Bai9_9 directory

--- Bai9_9/Bai9_9_1/index.js
+++
@@ -1,4 +0,0 @@
-let a=parseInt(prompt("nhap so a"));
-let b=parseInt(prompt("nhap so b:"));
-
-a%b==0? alert("a chia het cho b") : alert("a khong chia het cho b");
--- Bai9_9/Bai9_9_3/index.js
+++
@@ -1,2 +0,0 @@
-let number=parseInt(prompt("nhap so bat ky"));
-number<0?alert("so nho hon 0"):alert("so lon hon 0");
